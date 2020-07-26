99018dfbcbbf14457298d81d76b7cb15528027fe
nguyenhien112002 <nguyenhien112002@yahoo.com>
2020-07-26 21:57:25 +0700

    2

--- src/com/myclass/connection/JDBCConnection.java
+++ src/com/myclass/connection/JDBCConnection.java
@@ -7,22 +7,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBCConnection {
-	public static Connection getConnection() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/crm?serverTimezone=UTC", "root",
-					"Nh@090886");
-			return conn;
-			
-		} catch (ClassNotFoundException e) {
-			System.out.println("Không tìm thấy Driver");
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println("Kết nối thất bại");
-			e.printStackTrace();
-		} 
-		return null;
-	}
-	
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/crm?serverTimezone=UTC", "root",
+                    "Nh@090886");
+            return conn;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Không tìm thấy Driver");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Kết nối thất bại");
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
